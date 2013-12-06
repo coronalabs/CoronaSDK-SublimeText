1e94e84286b82144cab94f94a83c0dbed42e70cb
Perry Clarke <dpclarke@riding.net>
2013-12-06 12:18:20 -0800

    Cleaned up

--- corona_docs.py
+++ corona_docs.py
@@ -12,7 +12,7 @@ import webbrowser
 import string
 import re
 import urllib
- 
+
 SEARCH_URL = "http://www.google.com/cse?cx=009283852522218786394%3Ag40gqt2m6rq&ie=UTF-8&q={search_term}&sa=Search#gsc.tab=0&gsc.q={search_term}&gsc.page=1"
 
 # Note "lfs", "socket", "sqlite3" are omitted because we don't have complete docs for those
@@ -20,7 +20,8 @@ LIBRARY_APIS = (
   "ads", "analytics", "audio", "credits", "crypto", "display", "easing",
   "facebook", "gameNetwork", "global", "graphics", "io", "json", "licensing",
   "math", "media", "native", "network", "os", "package", "physics", "sprite",
-  "store", "storyboard", "string", "system", "table", "timer", "transition", "widget" )
+  "store", "storyboard", "string", "system", "table", "timer", "transition", "widget")
+
 
 # Python version independent UrlEncode
 def UrlEncode(s):
@@ -29,60 +30,64 @@ def UrlEncode(s):
   except AttributeError:
     return urllib.quote_plus(s)
 
- 
+
 class CoronaDocsCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        s = self.view.sel()[0]
- 
-        # Expand selection to current "word"
-        start = s.a
-        end = s.b
- 
-        view_size = self.view.size()
-        terminator = ['\t', ' ', '\"', '\'', '(', '=']
- 
-        while (start > 0
-                and not self.view.substr(start - 1) in terminator
-                and self.view.classify(start) & sublime.CLASS_LINE_START == 0):
-            start -= 1
- 
-        while (end < view_size
-                and not self.view.substr(end) in terminator
-                and self.view.classify(end) & sublime.CLASS_LINE_END == 0):
-            end += 1
- 
-        # Note if the current point is on a Lua keyword (according to
-        # the .tmLanguage definition)
-        isLuaKeyword = self.view.match_selector(start,
-                "keyword.control.lua, support.function.lua, support.function.library.lua")
+  def is_visible(self):
+    s = self.view.sel()[0]
+    return self.view.match_selector(s.a, "source.lua - entity")
 
-        use_docset = self.view.settings().get("corona_sdk_use_docset", "public")
-        if use_docset in ['legacy', 'daily']:
-          docset = use_docset + "/"
-        else:
-          docset = ""
+  def run(self, edit):
+    s = self.view.sel()[0]
 
-        # Convert "word" under cursor to Corona Docs link, or a Lua docs link
-        page = self.view.substr(sublime.Region(start, end))
-        page = page.strip(string.punctuation)
+    # Expand selection to current "word"
+    start = s.a
+    end = s.b
 
-        # Look for an embedded period which, if the class name is one of ours,
-        # indicates we should look it up in the "library" section of the docs
-        # (unless the class member doesn't start with a lowercase letter in which
-        # case it's a constant and we'll have to just default to searching for it)
-        if (re.search("\w+\.[a-z]", page) is not None
-              and page.partition(".")[0] in LIBRARY_APIS):
-          page = page.replace(".", "/")
-          docUrl = "http://docs.coronalabs.com/" + docset + "api/library/" + page + ".html";
-        elif isLuaKeyword:
-          # Unfortunately, there's no search on the Lua docs site so we need to guess at
-          # an anchor (if it's not there, you go to the top of the page)
-          docUrl = "http://www.lua.org/manual/5.1/manual.html#pdf-" + page
-        else:
-          # We don't know what we're on, send them to the Corona Docs search page
-          page = UrlEncode(page)
-          docUrl = SEARCH_URL.format(search_term = page)
+    view_size = self.view.size()
+    terminator = ['\t', ' ', '\"', '\'', '(', '=']
 
-        # print("docURL : " + docUrl)
- 
-        webbrowser.open_new_tab(docUrl)
+    while (start > 0
+            and not self.view.substr(start - 1) in terminator
+            and self.view.classify(start) & sublime.CLASS_LINE_START == 0):
+        start -= 1
+
+    while (end < view_size
+            and not self.view.substr(end) in terminator
+            and self.view.classify(end) & sublime.CLASS_LINE_END == 0):
+        end += 1
+
+    # Note if the current point is on a Lua keyword (according to
+    # the .tmLanguage definition)
+    isLuaKeyword = self.view.match_selector(start,
+                                            "keyword.control.lua, support.function.lua, support.function.library.lua")
+
+    use_docset = self.view.settings().get("corona_sdk_use_docset", "public")
+    if use_docset in ['legacy', 'daily']:
+      docset = use_docset + "/"
+    else:
+      docset = ""
+
+    # Convert "word" under cursor to Corona Docs link, or a Lua docs link
+    page = self.view.substr(sublime.Region(start, end))
+    page = page.strip(string.punctuation)
+
+    # Look for an embedded period which, if the class name is one of ours,
+    # indicates we should look it up in the "library" section of the docs
+    # (unless the class member doesn't start with a lowercase letter in which
+    # case it's a constant and we'll have to just default to searching for it)
+    if (re.search("\w+\.[a-z]", page) is not None and
+       page.partition(".")[0] in LIBRARY_APIS):
+      page = page.replace(".", "/")
+      docUrl = "http://docs.coronalabs.com/" + docset + "api/library/" + page + ".html"
+    elif isLuaKeyword:
+      # Unfortunately, there's no search on the Lua docs site so we need to guess at
+      # an anchor (if it's not there, you go to the top of the page)
+      docUrl = "http://www.lua.org/manual/5.1/manual.html#pdf-" + page
+    else:
+      # We don't know what we're on, send them to the Corona Docs search page
+      page = UrlEncode(page)
+      docUrl = SEARCH_URL.format(search_term=page)
+
+    # print("docURL : " + docUrl)
+
+    webbrowser.open_new_tab(docUrl)
