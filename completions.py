3b3cf58be978a90152891260c4e42a1b30fe4c87
Tom Piercy <tap32@medschl.cam.ac.uk>
2014-05-22 15:51:25 +0100

    Add braces as word terminators

--- completions.py
+++ completions.py
@@ -194,7 +194,7 @@ class CoronaLabs:
     end = s.b
 
     view_size = view.size()
-    terminator = ['\t', ' ', '\"', '\'', ':', '=', '-', '+', '*', '/', '^', ',']
+    terminator = ['\t', ' ', '\"', '\'', ':', '=', '-', '+', '*', '/', '^', ',', '(', ')']
     while (start > 0
             and not view.substr(start - 1) in terminator
             and view.classify(start) & sublime.CLASS_LINE_START == 0):
